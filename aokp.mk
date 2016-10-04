dee3c4ee094587db4bf2a13f0c3daa90a60f4edd
STELIX <ssspinni@gmail.com>
2016-10-04 22:28:29 +0200

    Z00RD - AOKPize
    Change-Id: Id39a30d4212ffb97c936d4fdd5028a85fead7038
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/asus/Z00RD/full_Z00RD.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
-PRODUCT_NAME := cm_Z00RD
+PRODUCT_NAME := aokp_Z00RD
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z00RD
